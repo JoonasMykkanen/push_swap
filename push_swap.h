54a24f7bf58538d965fade0b85549a08415845ce
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2023-03-13 09:57:39 +0200

    maybe working now

--- push_swap.h
+++ push_swap.h
@@ -2,6 +2,7 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include "libft/libft.h"
 typedef struct t_stack
 {
 	int	*a;
@@ -18,8 +19,6 @@ typedef struct t_algo_data
 	int	size_avg;
 }	algo_data;
 
-// offset_a = montako kertaa A pitää siirtää että saadaan B luku oikeaan paikkaan
-// offset_b = montako kertaa B pitää siirtää että saadaan luku B päälimmäiseksi siirtoa A:han varten
 typedef	struct t_moves
 {
 	int		offset_ab;
@@ -29,6 +28,7 @@ typedef	struct t_moves
 	int		index;
 	int		dir_a;
 	int		dir_b;
+	int		state;
 }	moves;
 
 typedef struct t_least
@@ -41,21 +41,7 @@ typedef struct t_least
 	int		dir_b;
 }	least;
 
-
-
-
-
-# include "libft/libft.h"
-
-
-
-
-
 void	print_stack(stack *s);
-
-
-
-
 
 void	pa(stack *s);
 void	pb(stack *s);
@@ -69,6 +55,8 @@ void	rra(stack *s);
 void	rrb(stack *s);
 void	rrr(stack *s);
 void	sort(stack *s);
+void	fake_ra(stack *s);
+void	fake_rra(stack *s);
 void	clean_up(stack *s);
 void	onni_sort(stack *s);
 void	sort_algo(stack *s);
@@ -77,6 +65,7 @@ int		arr_size(char **arr);
 void	sort_medium(stack *s);
 int		find_smallest(stack *s);
 void	check_duplicates(stack *s);
+int 	find_biggest(stack *s);
 void	check_args(stack *s, int argc, char **argv);
 
 #endif
